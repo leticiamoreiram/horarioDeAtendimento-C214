16cc52098242a86524c8934b6d1d3cff04478b7f
leticiamoreiram <leticia.m@gec.inatel.br>
2023-10-04 21:22:45 -0300

    json com info dos horarios

---
+++ src/HorariosDeAtendimentoJson.js
@@ -0,0 +1,39 @@
+const HorariosDeAtendimentoJson = {
+  1: {
+      nomeDoProfessor: 'Renzo',
+      horarioDeAtendimento: '10:00',
+      periodo: 'Integral',
+      sala: '5',
+      predio: [1, 2, 3, 4, 6]
+  },
+  2: {
+      nomeDoProfessor: 'Chris',
+      horarioDeAtendimento: '17:30',
+      periodo: 'Integral',
+      sala: '18',
+      predio: [1, 2, 3, 4, 6]
+  },
+  3: {
+      nomeDoProfessor: 'Yvo',
+      horarioDeAtendimento: '15:30',
+      periodo: 'Noturno',
+      sala: '9',
+      predio: [1, 2, 3, 4, 6]
+  },
+  4: {
+      nomeDoProfessor: 'Karina',
+      horarioDeAtendimento: '08:00',
+      periodo: 'Noturno',
+      sala: '6',
+      predio: [1, 2, 3, 4, 6]
+  },
+  5: {
+      nomeDoProfessor: 'Marcelo',
+      horarioDeAtendimento: '19:00',
+      periodo: 'Integral',
+      sala: '21',
+      predio: [1, 2, 3, 4, 6]
+  }
+};
+
+module.exports = HorariosDeAtendimentoJson;
