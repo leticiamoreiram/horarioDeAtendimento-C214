bf5086e84cb3d8e7123fb0075416c7aaea279f55
leticiamoreiram <leticia.m@gec.inatel.br>
2023-10-06 16:54:14 -0300

    metodos de busca

---
+++ src/BuscasHorarioDeAtendimento.js
@@ -0,0 +1,44 @@
+const HorariosDeAtendimentoService = require('./HorariosDeAtendimentoService');
+
+class BuscasHorarioDeAtendimento {
+    constructor() {
+        this.horariosDeAtendimentoService = new HorariosDeAtendimentoService();
+    }
+
+    buscaNomeDoProfessorDoHorarioDeAtendimento(id) {
+        const professor = this.horariosDeAtendimentoService.busca(id);
+        return professor.nomeDoProfessor;
+    }
+
+    buscaPredioDoHorarioDeAtendimento(id) {
+        const professor = this.horariosDeAtendimentoService.busca(id);
+        return professor.predio;
+    }
+
+    buscaHorarioDoHorarioDeAtendimento(id) {
+        const professor = this.horariosDeAtendimentoService.busca(id);
+        return professor.horarioDeAtendimento;
+    }
+
+    buscaPeriodoDoHorarioDeAtendimento(id) {
+        const professor = this.horariosDeAtendimentoService.busca(id);
+        return professor.periodo;
+    }
+
+    buscaSalaDoHorarioDeAtendimento(id) {
+        const professor = this.horariosDeAtendimentoService.busca(id);
+        return professor.sala;
+    }
+
+    buscaNomeDoProfessorDoHorarioDeAtendimentoPorSala(sala) {
+        const professor = this.horariosDeAtendimentoService.buscaPorSala(sala);
+        return professor.nomeDoProfessor;
+    }
+
+    buscaPredioDoHorarioDeAtendimentoPorNome(nome) {
+        const professor = this.horariosDeAtendimentoService.buscaPorNome(nome);
+        return professor.predio;
+    }
+}
+
+module.exports = BuscasHorarioDeAtendimento;
